34606309113ae2249d3afb51b92a81883bf2f0b4
Bob Tribbia <btribbia@newrelic.com>
2017-06-03 10:56:55 -0700

    changed test client to use room api instead of game api

--- client/js/app.js
+++ client/js/app.js
@@ -1,23 +1,23 @@
 
 
 var api = {
-	getGames: function() {
-		logStatus('getting games...');
-		$.getJSON( "api/games/", function( data ) {
+	getRooms: function() {
+		logStatus('get rooms...');
+		$.getJSON( "api/rooms/", function( data ) {
 			logStatus('games retreived.');
 			logScript(JSON.stringify(data));
 		})
 	},
-	postGames: function(data) {
-		logStatus('posting games...');
-		var payload = {"players":data};
+	joinRoom: function(playerName, roomId) {
+		logStatus('join room');
+		var payload = {"name":playerName};
 		$.post({
-		    url: "api/games/",
+		    url: "api/room/" + roomId + "/join",
 		    data: JSON.stringify(payload),
 		    contentType: "application/json; charset=utf-8",
 		    dataType: "json",
 		    success: function(result,status,xhr){
-		    	logStatus('game created');
+		    	logStatus('room joined');
 		    	logScript(JSON.stringify(result));
 		    },
 		    failure: function(errMsg) {
@@ -28,11 +28,13 @@ var api = {
 }
 
 
-function createDummyGame() {
-	var players = ["player1","player2"];
-	api.postGames(players);
+function join1Room() {
+	api.joinRoom('Player1','90414e7e-ad08-4647-97ad-d5880aefb6af');
 }
 
+function join2Room() {
+	api.joinRoom('Player2','90414e7e-ad08-4647-97ad-d5880aefb6af');
+}
 
 function logScript(msg) {
 	document.getElementById('scriptLog').innerHTML = msg;
@@ -42,5 +44,6 @@ function logStatus(msg) {
 	document.getElementById('status').innerHTML = msg;
 }
 
-document.getElementById("getGames").onclick = api.getGames;
-document.getElementById("postGames").onclick = createDummyGame;
+document.getElementById("getGames").onclick = api.getRooms;
+document.getElementById("p1join").onclick = join1Room;
+document.getElementById("p2join").onclick = join2Room;
